de2b65cd5f25f58298724749e37b2533c8a3ba51
Shelteret <seagbemaple12@>
2022-06-02 13:01:38 -0700

    Unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <unistd.h>
 /**
  * main - Printing quote
  *
@@ -7,6 +6,8 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 59);
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, str, 59);
 	return (1);
 }
