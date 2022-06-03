cf2ce032cd6d331b8d4426d8c05b1b13208b7a53
Shelteret <seagbemaple12@>
2022-06-03 06:54:14 -0700

    Variables, if, else, while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - Prints alphabtets in lowercase
+ *
+ * Return: Always 0.
+*/
+
+int main(void)
+{
+	char(letter);
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - prints the lowercase alphabet in reverse,
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char lowwecase;
+
+	for (lowercase = 'z'; lowercase >= 'a'; lowercase--)
+		putchar(lowercase)
+		putchar('\n');
+	return (0);
+}
