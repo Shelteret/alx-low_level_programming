4bcb331ac7eb10150689358f7f8dcf6f0cc3903a
Shelteret <seagbemaple12@>
2022-06-07 15:06:50 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet in lowercase
+ */
+void print_alphabet_x10(void)
+{
+int count = 0;
+char low;
+
+while (count++ <= 9)
+{
+for (low = 'a'; low <= 'z'; low++)
+_putchar(low);
+_putchar('\n');
+}
+}
