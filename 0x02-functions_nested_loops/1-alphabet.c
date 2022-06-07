dbd88ea4b20a0dc5d294bc52377a97d3977701c7
Shelteret <seagbemaple12@>
2022-06-07 13:56:02 -0700

    Lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - prints the alphabet, in lowercase, followed by a new line
+ *
+ * Return - 0 if success
+ */
+void print_alphabet(void);
+{
+	char low;
+
+	for (low = 'a'; low = 'z'; low++)
+	{
+		_putchar(low);
+	}
+
+	_putchar('\n');
+}
