c27a0307a11541a81cdc416296e79726cc10ab3b
Shelteret <seagbemaple12@>
2022-06-08 12:53:46 -0700

    Positive or negative test

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - tests for the sign of a given integer
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	a = 0;
+	positive_or_negative(a);
+
+	return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int highest_number(int j, int k, int l);
+int time_measure(int m, int d);
+void print_remaining_days(int m, int d, int y);
+
+#endif /* MAIN_H */
