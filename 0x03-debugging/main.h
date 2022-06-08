df30cbdaa87c6fae79e289f12d4f778aed0e105f
Shelteret <seagbemaple12@>
2022-06-08 13:00:38 -0700

    Positive or negative

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,9 +3,9 @@
 
 #include <stdio.h>
 
-void positive_or_negative(int i);
-int highest_number(int j, int k, int l);
-int time_measure(int m, int d);
+void positive_or_negative(int n);
+int largest_number(int j, int k, int l);
+int convert_day(int m, int d);
 void print_remaining_days(int m, int d, int y);
 
 #endif /* MAIN_H */
