93b373c91c58aff1cbd9922b4eb5e195197eb833
Shelteret <seagbemaple12@>
2022-06-07 18:00:55 -0700

    100-times_table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_times_table - function that prints the n times table, starting with 0.
+ * @a: input
+ * Return: Always 0 (Success)
+ */
+void print_times_table(int a)
+{
+	return (0);
+}
