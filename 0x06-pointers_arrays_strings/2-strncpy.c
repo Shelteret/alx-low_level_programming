12d9d2bf9c777c4f493fe90bc5c8b008a5e118ff
Shelteret <seagbemaple12@>
2022-06-16 05:52:31 -0700

    Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,11 +11,10 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int c;
 
-	for (c = 0; src[c] != '\0'; c++)
-		if (c < n)
-			dest[c] = src[c];
-		while (c < n)
-			dest[c++] = '\0';
+	for (c = 0; c < n && src[c] != '\0'; c++)
+		dest[c] = src[c];
+	for ( ; c < n; c++)
+		dest[c] = '\0';
 
 	return (dest);
 }
