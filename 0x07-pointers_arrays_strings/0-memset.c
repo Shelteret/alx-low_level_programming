5e7e6670b5589ce9518a5877d68085650fa113b7
Shelteret <seagbemaple12@>
2022-06-20 10:01:07 -0700

    Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,12 +10,11 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int c = 0;
+	unsigned int c;
 
-	while (c < n)
+	for (c = 0; c < n; c++)
 	{
 		s[c] = b;
-		c++;
 	}
 	return (s);
 }
