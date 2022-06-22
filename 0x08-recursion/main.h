caa93561beaa7a5f56267160baaac9a2a53db039
Shelteret <seagbemaple12@>
2022-06-22 08:24:50 -0700

    Task 0

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 int _putchar(char c);
 void _puts_recursion(char *s);
@@ -12,4 +12,4 @@ int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 
-#endif /* MAIN_H */
+#endif
