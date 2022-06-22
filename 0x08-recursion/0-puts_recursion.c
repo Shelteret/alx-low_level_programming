02e1c5eb1a631b2369dd2fb97cbf515439cde2bf
Shelteret <seagbemaple12@>
2022-06-22 04:03:50 -0700

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,16 +3,16 @@
 /**
  * _puts_recursion - function that prints a string
  * @s: Input
- * Return: Always 0
+ * Return: nothing
  */
 
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-		_putchar('\n');
-	else
+	if (*s != '\0')
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
+	else
+		_putchar('\n');
 }
