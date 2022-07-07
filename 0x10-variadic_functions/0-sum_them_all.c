4e9eefd466f395403099d8601dbeffd6e9929b00
Shelteret <seagbemaple12@>
2022-07-07 07:19:52 -0700

    Task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -15,7 +15,7 @@ int sum_them_all(const unsigned int n, ...);
 		return (0);
 
 	va_start(list, n);
-	for  (i = 0; i < n; i++)
+	for (a = 0; a < n; a++)
 		sum += va_arg(list, int);
 
 	va_end(list);
